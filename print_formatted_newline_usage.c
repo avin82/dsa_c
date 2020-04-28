f7e2d520b0340a2d26dc5b22df7f2a68e10900d6
Arvind Sharma <avin82.sharma@gmail.com>
2020-04-28 16:45:45 -0400

    Adding new file print_formatted_newline_usage.c

---
+++ print_formatted_newline_usage.c
@@ -0,0 +1,9 @@
+// Program to display print formatted newline usage
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello world\nThis is Arvind\nAnd I am learning to program in C\n");
+
+	return 0;
+}
