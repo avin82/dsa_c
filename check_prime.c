46ccd825c43803af1675bc840dbb4eb6141e0af7
Arvind Sharma <avin82.sharma@gmail.com>
2020-04-30 01:07:33 -0400

    Updating file check_prime.c

--- check_prime.c
+++ check_prime.c
@@ -9,8 +9,18 @@ int main(){
 	printf("Enter a number: ");
 	scanf("%d", &a);
 
+	if (a < 2){
+		printf("Not prime");
+		exit(0);
+	}
+	
+	if (a == 2){
+		printf("Prime");
+		exit(0);
+	}
+
 	for (i=2; i<a; i++){
-		if (a%i==0){
+		if (a%i == 0){
 			printf("Not prime, divisible by %d", i);
 			exit(0);
 		}
