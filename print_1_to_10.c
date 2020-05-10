d429ede3064f52f13226a58908402bd7311e14a9
Arvind Sharma <avin82.sharma@gmail.com>
2020-05-10 16:18:54 -0400

    Adding new file print_1_to_10.c

---
+++ print_1_to_10.c
@@ -0,0 +1,14 @@
+/* Program to print numbers from 1 to 10 using while loop */
+
+#include <stdio.h>
+
+int main(){
+	int i=1;
+	while (i <= 10){
+		printf("%d\t", i);
+		i = i + 1;	// Statement that changes the value of condition
+	}
+	printf("\n");
+
+	return 0;
+}
