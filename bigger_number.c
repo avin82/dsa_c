17183c66fc170df97a3e28cdb8d0cbc6405454fe
Arvind Sharma <avin82.sharma@gmail.com>
2020-05-10 12:29:40 -0400

    Adding new file bigger_number.c

---
+++ bigger_number.c
@@ -0,0 +1,15 @@
+/* Program to print the bigger number */
+
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	printf("Enter two numbers: ");
+	scanf("%d%d", &a, &b);
+	if (a>b)
+		printf("Bigger number=%d\n", a);
+	else
+		printf("Bigger number=%d\n", b);
+
+	return 0;
+}
