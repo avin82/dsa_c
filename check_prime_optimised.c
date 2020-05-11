defb415db1edc479a78d05061f03a91b5627ef83
Arvind Sharma <avin82.sharma@gmail.com>
2020-05-11 16:16:25 -0400

    Adding new file check_prime_optimised.c

---
+++ check_prime_optimised.c
@@ -0,0 +1,24 @@
+/* Program to find whether a number is prime or not (optimised) */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int i, n;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	if(n<2)
+		printf("%d is not prime\n", n);
+	else{
+		for(i=2; i<=sqrt(n); i++)
+			if(n%i==0)
+				break;
+	
+		if(i>sqrt(n))
+			printf("%d is prime\n", n);
+		else
+			printf("%d is not prime\n", n);
+	}
+	
+	return 0;
+}
